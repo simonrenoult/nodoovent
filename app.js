99bc0e2b8ace3210ba6aefe81d730ddbaf3dadd3
Simon RENOULT <g4llic4@gmail.com>
2012-05-11 19:35:23 +0700

    Update comments

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
-﻿var http = require ( 'http' )
+﻿/*
+ * Require the node modules. 
+ *  HTTP is a native nodejs module.
+ *  mysql is installed from Node Package Manager (npm),
+  * already present in node_modules folder.
+ */
+var http = require ( 'http' )
   , Client = require ( 'mysql' ).Client;
 
-var client = new Client ( );
-    client.user = '';
-    client.password = '';
-    client.host = '';
-    client.port = '';
-    
+/*
+ * Create a new mysql database client. You must specified the user, 
+ * password, host and port values.
+ */
+var dbClient = new Client ( );
+    dbClient.user = '';
+    dbClient.password = '';
+    dbClient.host = '';
+    dbClient.port = '';
+
+/*
+ * Create the node.js server.
+ * 
+ * req is an HTTP.Request object, containing all the request user informations.
+ * res is an HTTP.Response object, used to answer to the client. 
+ *
+ */
 http.createServer ( function ( req, res ) {
   res.writeHead( 200, {'Content-Type': 'text/plain'} );
   res.end( 'Hello World\n' );
 } ).listen ( 1337, '127.0.0.1' );
 
-client.query ( 'USE simonren_nodoovent' );
-
-client.query ( 
+/*
+ * Handle the database queries.
+ */
+dbClient.query ( 'USE simonren_nodoovent' );
+dbClient.query ( 
   'SELECT * FROM author',
   function selectAll ( err, res, fields ) {
     if ( err )
@@ -24,4 +43,7 @@ client.query (
   }
 );
 
+/*
+ * Display informations in the console.
+ */
 console.log ( 'Server running at http://127.0.0.1:1337/ ');
