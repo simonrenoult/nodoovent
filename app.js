21ecf46f89b07364be5d27af25c13479f7393555
Simon RENOULT <g4llic4@nuxtop.(none)>
2012-05-12 12:59:42 +0700

    updating .gitignore

--- app.js
+++ app.js
@@ -2,48 +2,13 @@
  * Require the node modules. 
  *  HTTP is a native nodejs module.
  *  mysql is installed from Node Package Manager (npm),
-  * already present in node_modules folder.
+ * already present in node_modules folder.
  */
-var http = require ( 'http' )
-  , Client = require ( 'mysql' ).Client;
+var appConf = require ( './lib/config.js' ) ( './conf.json' )
+  , router = require ( './lib/router.js' )
+  , handler = require ( './lib/handler.js' )
+  , server = require ( './lib/server.js' ) ( router, handler );
 
-/*
- * Create a new mysql database client. You must specified the user, 
- * password, host and port values.
- */
-var dbClient = new Client ( );
-    dbClient.user = '';
-    dbClient.password = '';
-    dbClient.host = '';
-    dbClient.port = '';
+server.listen ( appConf.api.port, appConf.api.address );
 
-/*
- * Create the node.js server.
- * 
- * req is an HTTP.Request object, containing all the request user informations.
- * res is an HTTP.Response object, used to answer to the client. 
- *
- */
-http.createServer ( function ( req, res ) {
-  res.writeHead( 200, {'Content-Type': 'text/plain'} );
-  res.end( 'Hello World\n' );
-} ).listen ( 1337, '127.0.0.1' );
-
-/*
- * Handle the database queries.
- */
-dbClient.query ( 'USE simonren_nodoovent' );
-dbClient.query ( 
-  'SELECT * FROM author',
-  function selectAll ( err, res, fields ) {
-    if ( err )
-      throw err;
-    else
-      console.log(res);
-  }
-);
-
-/*
- * Display informations in the console.
- */
-console.log ( 'Server running at http://127.0.0.1:1337/ ');
+console.log ( 'Server running at http://' + appConf.api.address + ':' + appConf.api.port );
