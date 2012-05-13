10a2a17ae405b5f346b1ee7d7a482fb61dc272ab
G4llic4 <g4llic4@gmail.com>
2012-05-13 02:02:46 +0200

    update routes and db conf

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 ﻿/*
  * Require the node modules. 
- *  HTTP is a native nodejs module.
- *  mysql is installed from Node Package Manager (npm),
+ * HTTP is a native nodejs module.
+ * mysql is installed from Node Package Manager (npm),
  * already present in node_modules folder.
  */
 var appConf = require ( './lib/config.js' ) ( './conf.json' )
+  , mysql = require ( 'mysql' )
+  , db = require ( './lib/db/index.js' ) ( mysql.Client, appConf.db )
   , router = require ( './lib/router.js' )
-  , handler = require ( './lib/handler.js' )
+  , handler = require ( './lib/handler.js' ) ( db )
   , server = require ( './lib/server.js' ) ( router, handler );
 
 server.listen ( appConf.api.port, appConf.api.address );
