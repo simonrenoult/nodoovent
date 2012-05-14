caa526e9e943af9335fc50ce90a5ef89b0372503
Simon RENOULT <g4llic4@gmail.com>
2012-05-14 21:43:17 +0700

    Adding node-orm and author table

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 ﻿/*
- * Require the node modules. 
+ * Require the node modules.
  * HTTP is a native nodejs module.
  * mysql is installed from Node Package Manager (npm),
  * already present in node_modules folder.
  */
 var appConf = require ( './lib/config.js' ) ( './conf.json' )
-  , mysql = require ( 'mysql' )
-  , db = require ( './lib/db/index.js' ) ( mysql.Client, appConf.db )
-  , router = require ( './lib/router.js' )
+  , orm = require ( 'orm' )
+  , db = require ( './lib/db/index.js' ) ( orm, appConf.db )
   , handler = require ( './lib/handler.js' ) ( db )
+  , router = require ( './lib/router.js' )
   , server = require ( './lib/server.js' ) ( router, handler );
 
 server.listen ( appConf.api.port, appConf.api.address );
 
-console.log ( 'Server running at http://' + appConf.api.address + ':' + appConf.api.port );
+console.log ( 'Server running at http://' + appConf.api.address + ':' + appConf.api.port + '.');
