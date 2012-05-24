a1419b7496f92693bb95e4ead39cef4dad895261
Simon RENOULT <g4llic4@gmail.com>
2012-05-25 00:49:52 +0700

    Project rewriting. All basics CRUD actions are availalbe through REST methods (GET, POST, PUT, DELETE) on each table.

--- app.js
+++ app.js
@@ -6,18 +6,15 @@
  */
 var http = require ( 'http' )
   , orm = require ( 'orm' )
-  , conf = require ( './lib/config.js' ) ( './conf.json' );
-
- http
-    .createServer ( function ( req, res) {
-       router ( req, res, db );
-    } )
-    .listen ( conf.api.port, conf.api.address );
+  , conf = require ( './lib/config.js' ) ( './conf.json' )
+  , models = require ( './lib/models.js' )
+  , server = require ( './lib/server.js' )
+  , router = require ( './lib/routes/_index.js' )
+  , handler = require ( './lib/handler.js' );
 
 /*
  * Connect orm to db
  */
-
 
 var connectionAddress =  conf.db.type + '://'
     + conf.db.user + ':' + conf.db.password
@@ -27,98 +24,13 @@ orm.connect ( connectionAddress, function ( success, db ) {
     if ( !success ) {
         console.log ( 'Could not connect to database.' );
     } else {
-
         console.log ( 'Application connected to database.' );
         console.log ( connectionAddress );
 
-        /*
-         * Define models
-         */
+        models.initialize ( db );
 
-        var Author = db.define ( "author", {
-            "aut_name"   : { "type": "string" }
-            }, {
-            "methods" : {
-                "fullName" :function () {
-                    return this.aut_name;
-                }
-            }
-        });
-
-        var Category = db.define("category", {
-            "cat_name"   : { "type": "string" },
-            "cat_description"   : { "type": "string" }
-        });
-
-        var Tag = db.define("tag", {
-            "tag_name"   : { "type": "string" }
-        });
-
-        var Element = db.define("element", {
-            "ele_name"   : { "type": "string" },
-            "ele_content"   : { "type": "string" },
-            "ele_data_creation"   : { "type": "date" }
-        });
-
-        Element.hasMany ( "authors", db._models.author, "author" );
-        Element.hasMany ( "tags", db._models.tag, "tag" );
-        Element.hasOne ( "category", db._models.category );
-
-        Tag.sync ( );
-        Category.sync ( );
-        Author.sync ( );
-        Element.sync ( );
-
-        /*
-         * Once models are defined, we can create a server and use models.
-         */
-
-        http
-            .createServer ( function ( req, res) {
-                router ( req, db, function ( result ) {
-                    res.writeHead ( 200, { "Content-Type": "text/plain" } );
-                    res.end ( result );
-                } );
-                console.log ( req );
-            } )
-            .listen ( conf.api.port, conf.api.address );
+        server.create ( db, router, handler ).listen ( conf.api.port, conf.api.address );
     }
 } );
-
-/*
- * Create a new server.
- * Each request is handled with the callback.
- */
-
-function router ( req, db, handler ) {
-
-    console.log ( req.method + ' ' + req.url );
-
-    if ( req.method === 'GET' ) {
-        var occ;
-        if ( occ = req.url.match ( /^\/author\/([0-9]+)/ ) ) {
-            db._models.author.get ( occ[1], function ( apiRes ) {
-                handler ( apiRes );
-            } );
-        }
-    } else if ( req.method === 'POST' ) {
-        var data = '';
-        req.on ( 'data', function ( chunk ) {
-            data += chunk;
-        } )
-        .on ( 'end', function ( ) {
-            if ( req.url.match ( /^\/author/ ) ) {
-                handler ( { OK: true } );
-            }
-        } )
-        .on ( 'error', function ( err ) {
-            handler ( { OK: false } );
-        } );
-    } else if ( req.method === 'PUT' ) {
-
-    } else if ( req.metod === 'DELETE' ) {
-
-    }
-}
 
 console.log ( 'Server running at http://' + conf.api.address + ':' + conf.api.port + '.');
