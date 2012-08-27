1ffa39ca6beca4539adb62c293cd34bf705a9a41
Simon RENOULT <simon.renoult@gmail.com>
2012-08-28 01:10:32 +0200

    Add testing argument (still need some work)

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
  * Require the node modules.
  * orm is installed from npm, already present in node_modules folder.
  */
-var sequelize = require ( 'sequelize' )
+var Sequelize = require ( 'sequelize' )
   , conf = require ( './lib/config.js' ) ( './conf.json' )
   , models = require ( './lib/models' )
+  , handler = require ( './lib/handler' )
   , router = require ( './lib/routes' )
-  , handler = require ( './lib/handler.js' )
   , server = require ( './lib/server.js' );
 
 /**
@@ -14,21 +14,36 @@ var sequelize = require ( 'sequelize' )
  * If connection is successful, initialize the models and create a server.
  *
  * @param conf {Object} Configuration file object.
- * @param sequelize {Object} Orm module.
+ * @param Sequelize {Object} Orm module.
  * @param {Function} Called if db connection succeeds.
  */
-models.connectOrm ( conf, sequelize, function ( dbConnection ) {
-    var dbObjects = models.initialize ( dbConnection );
+models.connectOrm ( conf, Sequelize, function ( sequelize ) {
+    var modelsObject = models.initialize ( sequelize );
     
-    // Create an instance of the HTTP server.
-    server.create ( conf.api, function ( req, res ) {
-        // Execute the router function each time the server is requested.
-        router ( conf.routes, req, handler, dbObjects, function ( result ) {
-            res.writeHead ( result.code, { "Content-Type": "application/json" } );
-            res.end ( JSON.stringify ( result.content ) );
-        } ); 
-    } );
+    var testSession = false;
+    for ( var i = 0 ; i < process.argv.length ; i++ ) {
+    	if ( process.argv[i] === '--test' || process.argv[i] === '-t' ) {
+    		testSession = true;
+    	}
+    }
 
     console.log ( 'API running at http://'
             + conf.api.address + ':' + conf.api.port + '.' );
+            
+    if ( !testSession ) {
+		// Create an instance of the HTTP server.
+		server.create ( conf.api, function ( req, res ) {
+		    // Execute the router function each time the server is requested.
+		    router ( conf.resources, req, handler, modelsObject, function ( result ) {
+		        res.writeHead ( result.code, { "Content-Type": "application/json" } );
+		        res.end ( JSON.stringify ( result.message ) );
+		    } );
+		} );
+	} else {
+		// Wait 500ms to be sure the shell is free.
+		setTimeout ( function ( ) {
+			var elements = require ( __dirname + '/tests/elements.tests.js' ) ( conf, modelsObject.elements, handler );
+		}, 500 );
+	}
+	
 } );
